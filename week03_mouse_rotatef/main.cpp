6c2ca2cea63c8f568c2f6591caf322333f8a2058
papayaa006 <114202036+papayaa006@users.noreply.github.com>
2024-03-14 16:31:11 +0800

    week03

---
+++ week03_mouse_rotatef/main.cpp
@@ -0,0 +1,35 @@
+#include <GL/glut.h>///�ϥ�GLIUT���~��
+#include <stdio.h>
+float teapotX=0, teapotY=0;
+float angle=0;
+void mouse(int button, int state, int x, int y)
+{
+    teapotX=(x-150)/150.0;
+    teapotY=-(y-150)/150.0;
+}
+void display()
+{
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glPushMatrix();
+    glPopMatrix();
+        ///glTranslatef(teapotX, teapotY, 0);
+        glRotatef(angle, 0, 0, 1);
+    glutSolidTeapot( 0.3 );
+    glutSwapBuffers();
+}
+void motion(int x, int y)
+{
+    angle=x;
+    display();
+}
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("week03_mouse");
+    glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
+    glutMainLoop();
+}
